7357f3ec24a9543654dff542afe64abfbdd9cd66
Rohit <javarohit@gmail.com>
2020-07-09 11:28:02 +0530

    DataSet

--- src/main/java/spark/RDD_wordcout.java
+++ src/main/java/spark/RDD_wordcout.java
@@ -1,30 +1,39 @@
-//package spark;
-//
-//import org.apache.log4j.Level;
-//import org.apache.log4j.Logger;
-//import org.apache.spark.SparkConf;
-//import org.apache.spark.api.java.JavaRDD;
-//import org.apache.spark.api.java.JavaSparkContext;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//
-//public class RDD_wordcout {
-//    public static void main(String a[]){
-//
-//        Logger.getLogger("org.apache").setLevel(Level.WARN);
-//
-//
-//        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
-//        JavaRDD<String> autoAllData = sc.textFile("E:\\work\\Spark\\src\\main\\resources\\abc.txt",2);
-//
-//        JavaRDD count = autoAllData.flatMap(line->line.split(" ")).map(word);
-//
-//        //JavaRDD<Integer> count = autoAllData.flatMap(line => line.split(" ")).map(word => (word,1)).reduceByKey(_+_) counts.collect();
-//
-//
-//
-//        sc.close();
-//    }
-//}
+package spark;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+import java.util.Arrays;
+
+
+public class RDD_wordcout {
+    public static void main(String arg[]){
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> lines = sc.textFile("E:\\work\\Spark\\src\\main\\resources\\movietweets.csv");
+        JavaPairRDD<String, Integer> counts = lines
+                .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((a, b) -> a + b);
+        //counts.saveAsTextFile("target/My_Data/abcOutput.txt");
+        counts.collect().forEach(System.out::println);
+        while (true){
+            try{
+                Thread.sleep(10000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        ///sc.close();
+    }
+}
