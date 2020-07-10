05d6a01f45c0ed76448049d85a9be7357dd0c114
Rohit <javarohit@gmail.com>
2020-07-10 12:45:12 +0530

    Streaming

--- src/main/java/spark/streaming/SparkStreamingDemo.java
+++ src/main/java/spark/streaming/SparkStreamingDemo.java
@@ -1,4 +1,5 @@
 package com.manish.SparkStreaming;
+
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -20,72 +21,76 @@ import com.manish.SparkStreaming.ExerciseUtils;
 import scala.Tuple2;
 
 public class SparkStreamingDemo {
-	public static void main(String[] args) {
-		
-		
-		Logger.getLogger("org.apache").setLevel(Level.WARN);
-		
-		SparkConf conf = new SparkConf().setAppName("Spark_Streaming").setMaster("local[2]");
-			
-		JavaStreamingContext streamingContext 
-		= new JavaStreamingContext(conf, Durations.seconds(3));
-	
-	JavaReceiverInputDStream<String> lines 
-		= streamingContext.socketTextStream("localhost", 9000);
-	
-	lines.print();
-	// Split each line into words
-	JavaDStream<String> words = lines.flatMap(
-	  new FlatMapFunction<String, String>() {
-	    @Override public Iterator<String> call(String x) {
-	      return Arrays.asList(x.split(" ")).iterator();
-	    }
-	  });
-	
-	// Count each word in each batch
-	JavaPairDStream<String, Integer> pairs = words.mapToPair(
-	  new PairFunction<String, String, Integer>() {
-	    @Override public Tuple2<String, Integer> call(String s) {
-	      return new Tuple2<>(s, 1);
-	    }
-	  });
-	JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey(
-	  new Function2<Integer, Integer, Integer>() {
-	    @Override public Integer call(Integer i1, Integer i2) {
-	      return i1 + i2;
-	    }
-	  });
+    public static void main(String[] args) {
 
-	// Print the first ten elements of each RDD generated in this DStream to the console
-	
-	wordCounts.print();
-	
-//	Function2<Integer, Integer, Integer> reduceFunc = new Function2<Integer, Integer, Integer>() {
-//		  @Override public Integer call(Integer i1, Integer i2) {
-//		    return i1 + i2;
-//		  }
-//		};
-//		
-//	JavaPairDStream<String, Integer> windowedWordCounts 
-//		= pairs.reduceByKeyAndWindow(reduceFunc,
-//									Durations.seconds(15), 
-//									Durations.seconds(3));
-//	
-//	windowedWordCounts.print();
-//	
-//	System.out.println("Starting Streaming...");
-	
-	
-	streamingContext.start();
-	
-	try {
-		streamingContext.awaitTermination();
-	} catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-	
-	streamingContext.close();
-	
-	ExerciseUtils.hold();
-}
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("Spark_Streaming").setMaster("local[2]");
+
+        JavaStreamingContext streamingContext
+                = new JavaStreamingContext(conf, Durations.seconds(3));
+
+        JavaReceiverInputDStream<String> lines
+                = streamingContext.socketTextStream("localhost", 9000);
+
+        lines.print();
+        // Split each line into words
+        JavaDStream<String> words = lines.flatMap(
+                new FlatMapFunction<String, String>() {
+                    @Override
+                    public Iterator<String> call(String x) {
+                        return Arrays.asList(x.split(" ")).iterator();
+                    }
+                });
+
+        // Count each word in each batch
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(String s) {
+                        return new Tuple2<>(s, 1);
+                    }
+                });
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey(
+                new Function2<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer i1, Integer i2) {
+                        return i1 + i2;
+                    }
+                });
+
+        // Print the first ten elements of each RDD generated in this DStream to the console
+
+        wordCounts.print();
+
+        ///WINDOWING **********************
+        Function2<Integer, Integer, Integer> reduceFunc = new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer i1, Integer i2) {
+                return i1 + i2;
+            }
+        };
+        JavaPairDStream<String, Integer> windowedWordCounts
+                = pairs.reduceByKeyAndWindow(reduceFunc,
+                Durations.seconds(15),
+                Durations.seconds(3));
+
+        windowedWordCounts.print();
+
+        System.out.println("Starting Streaming...");
+
+
+        streamingContext.start();
+
+        try {
+            streamingContext.awaitTermination();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        streamingContext.close();
+
+        ExerciseUtils.hold();
+    }
 }
